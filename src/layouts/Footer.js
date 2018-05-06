865cb72e87d3d6c048a73038f617893c3c77be7a
Ross Whitehouse <ross@live.com>
2018-05-06 23:20:22 +0100

    Ammend footer link to font

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footer panel">
-            <div>Made with <a href="https://www.gatsbyjs.org/">Gatsby.js</a>. Font is <a href="https://fonts.google.com/specimen/PT+Mono">PT Mono</a>.</div>
+            <div>Made with <a href="https://www.gatsbyjs.org/">Gatsby.js</a>. Font is <a href="https://fonts.google.com/specimen/Space+Mono">Space Mono</a>.</div>
             <div>Say no to scroll-jacking.</div>
         </div>
     );
