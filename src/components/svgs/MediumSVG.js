6b28c3cf09c281e7c678f297ceab3a4c1f2a5681
Ross Whitehouse <ross@live.com>
2018-05-07 12:10:05 +0100

    Added SVGS

---
+++ src/components/svgs/MediumSVG.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MediumSVG = () => {
+  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit-3"><polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon><line x1="3" y1="22" x2="21" y2="22"></line></svg>;
+};
+
+export default MediumSVG;
