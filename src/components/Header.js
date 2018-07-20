ed51470e171cd7dfd58dd1e4bcb1bc7a0f0b68c8
Ross Whitehouse <ross@live.com>
2018-07-20 19:55:30 +0100

    Add components from layouts

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Intro from '../components/Intro';
+
+const Header = () => {
+    return (
+        <div className="header panel">
+            <Intro />
+        </div>
+    );
+}
+
+export default Header;
